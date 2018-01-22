0b7bcb8ad324ae27d9481a9d22900d905fc1e5fe
Jonathan Carmack <jonathan.carmack@gsa.gov>
2018-01-22 11:58:37 -0500

    All functions working

--- src/etl.py
+++ src/etl.py
@@ -121,7 +121,7 @@ def aggregate_events(filtered_events_df, mortality_df,feature_map_df, deliverabl
     columns=['patient_id', 'feature_id', 'feature_value']
     filtered_events_df_lab = filtered_events_df[filtered_events_df['event_id'].str.contains('LAB')]
     filtered_events_df_lab['event_id'] = filtered_events_df_lab['event_id'].map(feature_map_df.set_index('event_id')['idx'])
-    filtered_events_df_lab.dropna(subset=['value'])
+    filtered_events_df_lab = filtered_events_df_lab.dropna(subset=['value'])
     aggregated_events_lab = filtered_events_df_lab.groupby(['patient_id', 'event_id'], as_index=False).count()
     aggregated_events_lab.rename(columns={'event_id':'feature_id'}, inplace=True)
     aggregated_events_lab_max = aggregated_events_lab.groupby(['feature_id'], as_index=False).agg({"value":"max"})
@@ -134,7 +134,7 @@ def aggregate_events(filtered_events_df, mortality_df,feature_map_df, deliverabl
     continuous_events = filtered_events_df[filtered_events_df['event_id'].str.contains('DRUG') | filtered_events_df['event_id'].str.contains('DIAG')]
     continuous_events['event_id'] = continuous_events['event_id'].map(
         feature_map_df.set_index('event_id')['idx'])
-    continuous_events.dropna(subset=['value'])
+    continuous_events = continuous_events.dropna(subset=['value'])
     aggregated_events = continuous_events.groupby(['patient_id','event_id'], as_index=False).agg({"value":"sum"})
     aggregated_events.rename(columns={'event_id': 'feature_id', 'value':'feature_value'}, inplace=True)
     aggregated_events_max = aggregated_events.groupby(['feature_id'], as_index=False).agg({"feature_value":"max"})
@@ -142,6 +142,8 @@ def aggregate_events(filtered_events_df, mortality_df,feature_map_df, deliverabl
     merged['feature_value'] = merged['feature_value_x'] / merged['feature_value_y']
     merged = merged[columns]
     aggregated_events = pd.concat([merged_lab, merged])
+    #aggregated_events = aggregated_events.dropna()
+    #print(aggregated_events)
     aggregated_events.to_csv(deliverables_path + 'etl_aggregated_events.csv',
                              columns=['patient_id', 'feature_id', 'feature_value'], index=False)
 
@@ -166,9 +168,22 @@ def create_features(events, mortality, feature_map):
     2. mortality : Key - patient_id and value is mortality label
     '''
     patient_features = {}
-    mortality = {}
+    for index, row in aggregated_events.iterrows():
+        if not patient_features.get(row['patient_id']):
 
-    return patient_features, mortality
+            patient_features[row['patient_id']] = [(row['feature_id'], row['feature_value'])]
+        else:
+            patient_features[row['patient_id']].append((row['feature_id'], row['feature_value']))
+    mortality_dict = {}
+    for index, row in mortality.iterrows():
+        mortality_dict[row['patient_id']] = row['label']
+
+    for key in patient_features.keys():
+        if not key in mortality_dict:
+            mortality_dict[key] = 0
+
+
+    return patient_features, mortality_dict
 
 def save_svmlight(patient_features, mortality, op_file, op_deliverable):
     
@@ -186,16 +201,14 @@ def save_svmlight(patient_features, mortality, op_file, op_deliverable):
     Note: Please make sure the features are ordered in ascending order, and patients are stored in ascending order as well.     
     '''
     line = ''
-    for key, value in patient_features.iteritems():
-        line = str(int(key)) + ' ' + str(mortality[key]) + ' '
+    for key, value in sorted(patient_features.iteritems()):
+        line += str(int(key)) + ' ' + str(mortality[key]) + ' '
         value = sorted(value)
         for item in value:
             line += str(int(item[0])) + ":" + str(format(item[1], '.6f')) + ' '
         line += '\n'
-        #print(line)
     deliverable1 = open(op_file, 'wb')
     deliverable2 = open(op_deliverable, 'wb')
-    
     deliverable1.write(line)
     deliverable2.write(line)
 
